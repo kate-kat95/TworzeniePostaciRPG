70b29814e56f75837e507abc1db770e7328e638e
Olendroza <34488056+Olendroza@users.noreply.github.com>
2018-03-16 11:31:54 +0100

    Added fields to Postac

--- src/Postac.java
+++ src/Postac.java
@@ -1,4 +1,17 @@
 
 public class Postac {
+	
+	 String imie;
+	 int SumaWspolczynnikowGlownych;
+     int Rany = 0;
+     int[] WspolczynnikiGlowne= new int[8];
+     int[] WspolczynnikiPomocnicze = new int[5];
+     int[] Umietnosci = new int[22];
+     int[] AkcjeRuch = new int[4];
+     int[] AkcjeZwarcie = new int[2];
+     int[] AkcjeRapierAtak = new int[7];
+     int[] AkcjeRapierObrona = new int[4];
+     int[] AkcjeMieczAtak = new int [4];
+     int[] AkcjeMieczObrona = new int[3];
 
 }
