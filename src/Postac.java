dbfcf22cadbbd27c2ba781890035079f12c5fe24
kate-kat95 <37440047+kate-kat95@users.noreply.github.com>
2018-03-18 19:02:01 +0100

    coefficient without faith

--- src/Postac.java
+++ src/Postac.java
@@ -7,7 +7,7 @@ public class Postac {
      int Rany = 0;
      int[] WspolczynnikiGlowne= new int[8];
      int[] WspolczynnikiPomocnicze = new int[5];
-     int[] Umietnosci = new int[22];
+     int[] Umiejetnosci = new int[22];
      int[] AkcjeRuch = new int[4];
      int[] AkcjeZwarcie = new int[2];
      int[] AkcjeRapierAtak = new int[7];
@@ -20,8 +20,8 @@ public class Postac {
     	 // for testing purpose
     	 for(int i=0;i<this.WspolczynnikiPomocnicze.length;i++)
     		 this.WspolczynnikiPomocnicze[i]=0;
-    	 for(int i=0;i<this.Umietnosci.length;i++)
-    		 this.Umietnosci[i]=0;
+    	 for(int i=0;i<this.Umiejetnosci.length;i++)
+    		 this.Umiejetnosci[i]=0;
     	 for(int i=0;i<this.AkcjeRuch.length;i++)
     		 this.AkcjeRuch[i]=0;
     	 for(int i =0;i<this.AkcjeZwarcie.length;i++)
@@ -40,7 +40,8 @@ public class Postac {
     	 this.imie = "piotr";
     	 for(int i=0;i<this.WspolczynnikiGlowne.length;i++) {
     		 this.WspolczynnikiGlowne[i] = generator.nextInt(14)+6;
-    	 		}
+    		 SumaWspolczynnikowGlownych += WspolczynnikiGlowne[i];
+    	 }
     	 
     	//auxiliary coefficients
     	 WspolczynnikiPomocnicze[0]=(WspolczynnikiGlowne[1]+WspolczynnikiGlowne[3]+WspolczynnikiGlowne[5])/3;
@@ -49,9 +50,22 @@ public class Postac {
     	 WspolczynnikiPomocnicze[3]=(WspolczynnikiGlowne[0]+WspolczynnikiGlowne[1]+WspolczynnikiGlowne[2])/3;
     	 WspolczynnikiPomocnicze[4]=(WspolczynnikiGlowne[0]+WspolczynnikiGlowne[3]+WspolczynnikiGlowne[5])/3;
     	 
-    	 
+    	 //auxiliary coefficients without faith
+    	 int SumaUmiejetnosci=8;
+    	 for(int i = 0;i<22;i++) {
+    		 if(i<4) Umiejetnosci[i]=2;
+    		 else {
+    			 Umiejetnosci[i]=1;
+    			 SumaUmiejetnosci++;
+    		 }
+    	 }
 
+    	 while(SumaUmiejetnosci<=(int)(SumaWspolczynnikowGlownych/2)) {
+    		 int los = generator.nextInt(22);
+    		 Umiejetnosci[los]++;
+    		 SumaUmiejetnosci++;
+    	 }
     	 
-    	 
+    	 //determination of combat actions
      }
 }
