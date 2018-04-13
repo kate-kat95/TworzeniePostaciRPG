7f9f610163e2ba5a11a109f25ea1bf09a7495a6f
Olendroza <34488056+Olendroza@users.noreply.github.com>
2018-04-13 11:27:16 +0200

    Added zapisAndroid method

--- src/Startlosowania.java
+++ src/Startlosowania.java
@@ -19,11 +19,10 @@ class Postac {
     int[] AkcjeRapierObrona = new int[4];
     int[] AkcjeMieczAtak = new int [4];
     int[] AkcjeMieczObrona = new int[3];
-	 Random generator= new Random();
-	 
+    
     public void losujpostac() throws FileNotFoundException 
     {
-   
+   	 Random generator= new Random();
    	 
    	 //name of this creature 
    	 int plec= generator.nextInt(2);
@@ -179,69 +178,11 @@ for(int i=0;i<3 ;i++) {
 }
       
     }
-//healing or hurting
-	public void Rany(Boolean ranka) {
-		
-		if(ranka==false) {
-			for(int i=0;i<3;i++) {
-				AkcjeZwarcie[i]++;
-			}
-			for(int i=0;i<4;i++) {
-				AkcjeMieczObrona[i]++;
-			}
-			for(int i=0;i<5;i++) {
-				AkcjeRuch[i]++;
-				AkcjeRapierObrona[i]++;
-				AkcjeMieczAtak[i]++;
-			}
-			for(int i=0;i<6;i++) {
-				WspolczynnikiPomocnicze[i]++;
-			}
-			for(int i=0;i<8;i++) {
-				AkcjeRapierAtak[i]++;
-			}
-			for(int i=0;i<9;i++) {
-				WspolczynnikiGlowne[i]++;
-			}
-
-		}
-		else {
-			for(int i=0;i<3;i++) {
-				AkcjeZwarcie[i]--;
-			}
-			for(int i=0;i<4;i++) {
-				AkcjeMieczObrona[i]--;
-			}
-			for(int i=0;i<5;i++) {
-				AkcjeRuch[i]--;
-				AkcjeRapierObrona[i]--;
-				AkcjeMieczAtak[i]--;
-			}
-			for(int i=0;i<6;i++) {
-				WspolczynnikiPomocnicze[i]--;
-			}
-			for(int i=0;i<8;i++) {
-				AkcjeRapierAtak[i]--;
-			}
-			for(int i=0;i<9;i++) {
-				WspolczynnikiGlowne[i]--;
-			}
-		}
-	}
-	
-//refreshing auxiliary coefficients
-	public void odswiez() {
-	  	 WspolczynnikiPomocnicze[0]=(int)(WspolczynnikiGlowne[1]+WspolczynnikiGlowne[3]+WspolczynnikiGlowne[5])/3;
-	   	 WspolczynnikiPomocnicze[1]=(int)(WspolczynnikiGlowne[0]+WspolczynnikiGlowne[4]+WspolczynnikiGlowne[7])/3;
-	   	 WspolczynnikiPomocnicze[3]=(int)(WspolczynnikiGlowne[0]+WspolczynnikiGlowne[1]+WspolczynnikiGlowne[2])/3;
-	   	 WspolczynnikiPomocnicze[4]=(int)(WspolczynnikiGlowne[0]+WspolczynnikiGlowne[3]+WspolczynnikiGlowne[5])/3;
-	}
-	
 
 //SAvE YOUR CHARACTER! NEW IMPOSSIBLE OPTION! ONLY HERE XD
 		public void zapis() {
 					try {
-						PrintWriter zapis = new PrintWriter("zapisanapostac"+" "+ imie+".txt");
+						PrintWriter zapis = new PrintWriter("zapisanapostac.txt");
 						zapis.println(imie);
 						zapis.println(SumaWspolczynnikowGlownych);
 						zapis.println(Rany);
@@ -277,6 +218,143 @@ for(int i=0;i<3 ;i++) {
 							System.out.println("Niestety, nie mogê utworzyæ pliku!");
 					}
 		}
+}
+public Postac(String imionaM, String imionaZ) {
+    Random generator= new Random();
+
+    int plec= generator.nextInt(2);
+    String[] meskieimiona = imionaM.split("\\n");
+    String[] damskieimiona = imionaZ.split("\\n");
+    if(plec==0)
+        this.imie=meskieimiona[generator.nextInt(meskieimiona.length)];
+    else
+        this.imie=damskieimiona[generator.nextInt(damskieimiona.length)];
+
+    //main coefficients
+    for(int i=0;i<this.WspolczynnikiGlowne.length;i++) {
+        this.WspolczynnikiGlowne[i] = generator.nextInt(14)+6;
+        SumaWspolczynnikowGlownych += WspolczynnikiGlowne[i];
+    }
+
+    //auxiliary coefficients
+    WspolczynnikiPomocnicze[0]=(int)(WspolczynnikiGlowne[1]+WspolczynnikiGlowne[3]+WspolczynnikiGlowne[5])/3;
+    WspolczynnikiPomocnicze[1]=(int)(WspolczynnikiGlowne[0]+WspolczynnikiGlowne[4]+WspolczynnikiGlowne[7])/3;
+    WspolczynnikiPomocnicze[2]=20;
+    WspolczynnikiPomocnicze[3]=(int)(WspolczynnikiGlowne[0]+WspolczynnikiGlowne[1]+WspolczynnikiGlowne[2])/3;
+    WspolczynnikiPomocnicze[4]=(int)(WspolczynnikiGlowne[0]+WspolczynnikiGlowne[3]+WspolczynnikiGlowne[5])/3;
+    WspolczynnikiPomocnicze[5]=(int)WspolczynnikiGlowne[7]*5;
+    //auxiliary coefficients
+    int SumaUmiejetnosci=8;
+    for(int i = 0;i<Umiejetnosci.length;i++) {
+        if(i<4) Umiejetnosci[i]=2;
+        else {
+            Umiejetnosci[i]=1;
+            SumaUmiejetnosci++;
+        }
+    }
+
+    while(SumaUmiejetnosci<=(int)(SumaWspolczynnikowGlownych/2)) {
+        int los = generator.nextInt(22);
+        Umiejetnosci[los]++;
+        SumaUmiejetnosci++;
+    }
+
+    //determination of combat actions
+    AkcjeRuch[0] = WspolczynnikiPomocnicze[3] - 10;
+    AkcjeRuch[1] = WspolczynnikiPomocnicze[3] - 9;
+    AkcjeRuch[2] = WspolczynnikiPomocnicze[3] - 12;
+    AkcjeRuch[3] = (int)(WspolczynnikiPomocnicze[3]/3)+4;
+
+    for(int i=0;i<Umiejetnosci[1];i++) {
+        int los = generator.nextInt(4);
+        AkcjeRuch[los]++;
+    }
+
+    //movements
+    AkcjeRapierAtak[0] = (int)(WspolczynnikiPomocnicze[3] / 2) - 5;
+    AkcjeRapierAtak[1] = (int)(WspolczynnikiPomocnicze[3] / 2) - 5;
+    AkcjeRapierAtak[2] = WspolczynnikiPomocnicze[3] -15;
+    AkcjeRapierAtak[3] = (int)(WspolczynnikiPomocnicze[3] / 2) - 7;
+    AkcjeRapierAtak[4] = WspolczynnikiPomocnicze[3] -12;
+    AkcjeRapierAtak[2] = WspolczynnikiPomocnicze[3] -8;
+    AkcjeRapierAtak[5] = (int)(WspolczynnikiPomocnicze[3] / 3) - 1;
+
+    AkcjeRapierObrona[0] = (int)(WspolczynnikiPomocnicze[3]/2)-2;
+    AkcjeRapierObrona[1] = (int)(WspolczynnikiPomocnicze[3]/2)-2;
+    AkcjeRapierObrona[2] = (int)(WspolczynnikiPomocnicze[3]/2)-5;
+    AkcjeRapierObrona[3] = WspolczynnikiPomocnicze[3]-18;
+
+    for(int i=0;i<Umiejetnosci[2]*3;i++) {
+	 int los= generator.nextInt(11);
+	 	if(los<=6) AkcjeRapierAtak[los]++;
+	 	else {
+	 		los=los-7;
+	 		AkcjeRapierObrona[los]++;
+        }
+    }
+
+    //rapier
+    AkcjeMieczAtak[0] = (int)(WspolczynnikiPomocnicze[3] / 2) - 7;
+    AkcjeMieczAtak[1] = WspolczynnikiPomocnicze[3] - 17;
+    AkcjeMieczAtak[2] = (int)(WspolczynnikiPomocnicze[3] / 2) - 5;
+    AkcjeMieczAtak[3] = (int)(WspolczynnikiPomocnicze[3] / 3) - 3;
+
+    AkcjeMieczObrona[0] = (int)(WspolczynnikiPomocnicze[3] / 2) - 4;
+    AkcjeMieczObrona[1] = (int)(WspolczynnikiPomocnicze[3] / 2) - 4;
+    AkcjeMieczObrona[2] = WspolczynnikiPomocnicze[3] - 20;
+
+    for(int i=0;i<Umiejetnosci[3]/2;i++) {
+        int los = generator.nextInt(7);
+        if(los<4) AkcjeMieczAtak[los]++;
+        else {
+            los=los-4;
+            AkcjeMieczObrona[los]++;
+        }
+    }
+    //sword
+    AkcjeZwarcie[0] = (int)(WspolczynnikiPomocnicze[3] / 2) - 5;
+    AkcjeZwarcie[1] = (int)(WspolczynnikiPomocnicze[3] / 2) - 3;
+
+    for(int i=0;i<Umiejetnosci[0];i++) {
+        int los = generator.nextInt(2);
+        AkcjeZwarcie[los]++;
+    }
+}
+//zpis androidowy
+public String ZapiszAndroid() {
+
+String record ="";
+record+=this.imie+",";
+record+=this.SumaWspolczynnikowGlownych+",";
+record+=this.Rany+",";
+for(int i=0;i< 8;i++) {
+    record+=this.WspolczynnikiGlowne[i]+",";
+}
+for(int i=0;i<5 ;i++) {
+    record+=this.WspolczynnikiPomocnicze[i]+",";
+}
+for(int i=0;i< 22;i++) {
+    record+=this.Umiejetnosci[i]+",";
+}
+for(int i=0;i< 4;i++) {
+    record+=this.AkcjeRuch[i]+",";
+}
+for(int i=0;i<2 ;i++) {
+    record+=this.AkcjeZwarcie[i]+",";
+}
+for(int i=0;i< 7;i++) {
+    record+=this.AkcjeRapierAtak[i]+",";
+}
+for(int i=0;i< 4;i++) {
+    record+=this.AkcjeRapierObrona[i]+",";
+}
+for(int i=0;i<4 ;i++) {
+    record+=this.AkcjeMieczAtak[i]+",";
+}
+for(int i=0;i<3 ;i++) {
+    record+=this.AkcjeMieczObrona[i]+",";
+}
+return record;
 }
 
 public class Startlosowania {
